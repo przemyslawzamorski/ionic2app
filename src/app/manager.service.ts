c8c55bcebeb8ea83d88a35d5142b733e0ae212f3
Zamor <przemyslaw.zamorskii@gmail.com>
2016-12-05 18:57:07 +0100

    code reformat

--- src/app/manager.service.ts
+++ src/app/manager.service.ts
@@ -6,19 +6,18 @@ export class Manager {
     constructor(private http: Http) {
     }
 
-    // getRepos(username) {
-    //     let repos = this.http.get(`https://api.github.com/users/${username}/repos`);
-    //     return repos;
-    // }
+    public serverAdress: string = 'https://adoptuj-pupila.herokuapp.com/pl/api/v1/'
 
     getShelterList() {
-        let headers = new Headers({
-            'Content-Type': 'application/json', 
-            'Access-Control-Allow-Origin': "*"
-        });
-        // let headers = new Headers();
-        // headers.append('Content-Type', 'application/json');
-        // headers.append('Access-Control-Allow-Origin', "*"); 
+        // let headers = new Headers({
+        //     'Content-Type': 'application/json',
+        //     'Access-Control-Allow-Origin': "*"
+        // });
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Access-Control-Allow-Origin', "*"); 
+
+        // return this.http.get(this.serverAdress + 'shelters/', { headers: headers });
 
         return this.http.get(`/assets/shelters.json`, { headers: headers });
     }
